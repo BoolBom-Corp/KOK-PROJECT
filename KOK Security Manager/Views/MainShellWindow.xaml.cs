8d0375b6c6e00b1dd8329cf3053d515e08fcd458
Paweł Tatys <Pawel1695@gmail.com>
2020-01-29 17:59:08 +0100

    Dodanie user control MainWindowLeftMenuButton

--- KOK Security Manager/Views/MainShellWindow.xaml.cs
+++ KOK Security Manager/Views/MainShellWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace KOK_Security_Manager
+namespace KOK_Security_Manager.View
 {
     /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
+    /// Logika interakcji dla klasy MainShellWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainShellWindow : Window
     {
-        public MainWindow()
+        public MainShellWindow()
         {
             InitializeComponent();
         }
